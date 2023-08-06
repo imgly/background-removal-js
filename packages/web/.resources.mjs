0604c01f5242125a1e050cd6b87662a166531633
Daniel Hauschildt <daniel.hauschildt@img.ly>
2023-08-06 18:43:22 +0200

    refactor: Migrated to esbuild

--- packages/web/.resources.mjs
+++ packages/web/.resources.mjs
@@ -1,7 +1,12 @@
 export default [
   {
     path: '/models/',
-    source: '../../bundle/models/*',
+    source: '../../bundle/models/small',
+    mime: 'application/octet-steam'
+  },
+  {
+    path: '/models/',
+    source: '../../bundle/models/medium',
     mime: 'application/octet-steam'
   },
   {
