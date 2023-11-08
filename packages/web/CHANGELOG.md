f96258244277efb38160b4fba90bce3cf7f3e587
Daniel Hauschildt <daniel.hauschildt@img.ly>
2023-11-08 18:50:09 +0100

    wip

--- packages/web/CHANGELOG.md
+++ packages/web/CHANGELOG.md
@@ -1,18 +1,21 @@
 # Changelog
 
 All notable changes to this project will be documented in this file.
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [1.2.1]
 
-### Added
-
-- did something to the sdsad,
-
-## [1.2.0]
-
-Show me
+## 1.2.0
 
 ### Added
 
-- Hello did something to the sdsad,Hello did something to the sdsad1
+- Support `foreground`, `background` and `mask` export type.
+- Added support for `webp` and `jpeg` export formats.
+
+## 1.2.1
+
+### Added
+
+- Support `foreground`, `background` and `mask` export type
+
+## Unreleased
 
---
+++ scripts/changelog/create.mjs
@@ -0,0 +1,29 @@
+// Rails filename convention from migrations
+import fs from 'node:fs';
+import moment from 'moment';
+import path from 'node:path'
+
+const version = "Unreleased"
+const scriptDir = path.dirname(process.argv[1]);
+const templatePath = path.resolve(scriptDir, 'template.yml')
+const changeLogDir = path.resolve('./changelog', `${version}`);
+
+const name = process.argv[2]
+if (!name)  {
+  console.error('Please provide a name for your changelog entry');
+  process.exit()
+}
+
+
+
+const dateFormat = 'YYYYDDMMHHmmss'
+
+
+
+const date = Date.now()
+const prefix = moment(date).format(dateFormat)
+const filename = `${prefix}-${name}.yaml`
+
+// do it
+fs.mkdirSync(changeLogDir, { recursive: true });
+fs.copyFileSync(templatePath, path.resolve(changeLogDir, filename))
