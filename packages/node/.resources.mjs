bdb4771d52494557353f54342dfc759b2ea12a46
Daniel Hauschildt <daniel.hauschildt@img.ly>
2023-12-30 10:25:00 +0100

    fix: untouched node resources

--- packages/node/.resources.mjs
+++ packages/node/.resources.mjs
@@ -1,1 +1,12 @@
-export default [];
+export default [
+  {
+    path: '/models/',
+    source: '../../bundle/models/small',
+    mime: 'application/octet-steam'
+  },
+  {
+    path: '/models/',
+    source: '../../bundle/models/medium',
+    mime: 'application/octet-steam'
+  }
+];
